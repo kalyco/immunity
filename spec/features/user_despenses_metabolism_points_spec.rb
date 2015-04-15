d58f3c8147fef5f0f58490002dec43617fe40c56
Kayla <kayla.comalli@gmail.com>
2015-04-15 16:28:25 -0400

    testing user stories

---
+++ spec/features/user_despenses_metabolism_points_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+feature "user dispenses meta points" do
+  let(:user) { FactoryGirl.create(:user) }
+
+  scenario "user adds meta points to system" do
+    sign_in user
+    visit root_path
+    click_button "initialize immune system"
+    # add meta points
+    # click submit
+    expect(page).to have_content "meta points added"
+  end
+end
